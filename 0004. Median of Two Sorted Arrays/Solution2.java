17dbc5a115666d631d66928aacfbaaf25e726a01
durgeshgowdac <durgeshcgowda@gmail.com>
2024-01-14 02:02:57 +0530

    Median of Two Sorted Arrays

---
+++ 0004. Median of Two Sorted Arrays/Solution2.java
@@ -0,0 +1,28 @@
+class Solution2 {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length, n2 = nums2.length;
+        int[] result = new int[n1 + n2];
+
+        int i = 0, j = 0, k = 0;
+        while (i <= n1 && j <= n2) {
+            if (i == n1) {
+                while (j < n2) result[k++] = nums2[j++];
+                break;
+            }
+
+            if (j == n2) {
+                while (i < n1) result[k++] = nums1[i++];
+                break;
+            }
+
+            if (nums1[i] < nums2[j]) {
+                result[k++] = nums1[i++];
+            } else {
+                result[k++] = nums2[j++];
+            }
+        }
+
+        int mid = (n1 + n2) / 2;
+        return ((n1 + n2) % 2 == 0) ? (result[mid - 1] + result[mid]) / 2.0 : result[mid];
+    }
+}
